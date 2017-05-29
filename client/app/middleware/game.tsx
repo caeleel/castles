d7518c8d8504c9a2733c15036a47c538662ac833
Karl Jiang <karl@justin.tv>
2017-05-28 17:18:53 -0700

    move websocket out of echo functioN

--- client/app/middleware/game.tsx
+++ client/app/middleware/game.tsx
@@ -1,9 +1,10 @@
 import * as actions from '../actions/actions'
 import { Middleware } from 'redux'
 
+const ws = new WebSocket('ws://localhost:8080/');
+
 export const gameMiddleware: Middleware = (store: any) => (next: any) => (action: actions.Action<any>) => {
   if (actions.game.echo.matches(action)) {
-    const ws = new WebSocket('ws://localhost:8080/');
     ws.onopen = () => {
       ws.onmessage = (message: any) => {
         console.log(message.data);
