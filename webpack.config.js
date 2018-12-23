77e892074bb72af1907705caced3fc0d51baa9ec
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2018-12-23 20:34:13 +1100

    remove isometric-fetch package

--- webpack.config.js
+++ webpack.config.js
@@ -29,14 +29,6 @@ module.exports = function (env = "development") {
         // { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
       ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-      'isomorphic-fetch': 'fetch',
-    },
   };
 
   if (env == "development") {
